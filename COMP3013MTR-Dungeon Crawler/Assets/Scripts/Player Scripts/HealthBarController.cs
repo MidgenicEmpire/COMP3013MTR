3ee66009986d5fa1e68f7ef504d8cb542bd23b1a
SteelBoshido99 <55700855+SteelBoshido99@users.noreply.github.com>
2021-11-28 15:37:38 +0000

    MTR-34
    Health bar has been completed

--- COMP3013MTR-Dungeon Crawler/Assets/Scripts/Player Scripts/HealthBarController.cs
+++ COMP3013MTR-Dungeon Crawler/Assets/Scripts/Player Scripts/HealthBarController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class HealthBarController : MonoBehaviour
 {
     private PlayerHealth playerHealth;
+    GameObject healthBarObject;
+
+
     public void SetUp(PlayerHealth playerHealth)
     {
         this.playerHealth = playerHealth;
@@ -15,7 +18,7 @@ public class HealthBarController : MonoBehaviour
     //Updates player health only on event trigger
     private void PlayerHealth_OnHealthChanged(object sendder, System.EventArgs e)
     {
-        transform.Find("Fill").localScale = new Vector3(playerHealth.GetHealthPercent(), 1);
+        transform.Find("FillContainer").localScale = new Vector3(playerHealth.GetHealthPercent(), 1);
     }
 
     
