d4f3c60a56de43d72ee7a9925376a4549b68b86d
Michael <55700937+mriley-wallace@users.noreply.github.com>
2022-01-05 14:59:01 +0000

    MTR-72 Purchase works

---
+++ COMP3013MTR-Dungeon Crawler/Assets/Scripts/Shop System/ItemPurchase.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPurchase : MonoBehaviour
+{
+    public ItemObject item;
+
+
+    public void ItemToPurchase()
+    {
+        Inventory.instance.Add(item);
+    }
+}
