9a44fb836f317542f89c11adc132b0a514d60f47
SteelBoshido99 <55700855+SteelBoshido99@users.noreply.github.com>
2022-01-14 17:25:51 +0000

    MTR-107 Game Over
    Added new assets and sprites to the game over menu

--- COMP3013MTR-Dungeon Crawler/Assets/Scripts/MenuScripts/GameOver.cs
+++ COMP3013MTR-Dungeon Crawler/Assets/Scripts/MenuScripts/GameOver.cs
@@ -8,12 +8,20 @@ public class GameOver : MonoBehaviour
     // Start is called before the first frame update
     public void RestartGame()
     {
+        Debug.Log("Restarting Game");
         SceneManager.LoadScene("MainGame");
     }
 
     // Update is called once per frame
     public void ToMainMenu()
     {
+        Debug.Log("Going to main menu");
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitButton()
+    {
+        Debug.Log("Quitting application");
+        Application.Quit();
     }
 }
